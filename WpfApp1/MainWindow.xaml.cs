08f693a879a9769d1db20d3ef455f6113a13f253
宗彥 黃 <5b0g0017@office.stust.edu.tw>
2023-09-23 17:15:26 +0800

    2

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using static WpfApp1.MainWindow;
 
 namespace WpfApp1
 {
@@ -13,65 +16,69 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
-
+        public class Triangle
+        {
+            public double Side1 { set;  get; }
+            public double Side2 { set;  get; }
+            public double Side3 { set; get; }
+            public bool IsValid { set; get; }
+        }
+        List<Triangle> triangles=new List<Triangle>();
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            int number;
-            List<int> primes = new List<int>();
-            bool success = int.TryParse(textbox1.Text, out number);
+            double side1, side2, side3;
+            bool success1 = double.TryParse(textbox1.Text, out side1);
+            bool success2 = double.TryParse(textbox2.Text, out side2);
+            bool success3 = double.TryParse(textbox3.Text, out side3);
 
-            if (!success)
+            // 清除之前可能设置的边框颜色
+            textbox1.ClearValue(TextBox.BorderBrushProperty);
+            textbox2.ClearValue(TextBox.BorderBrushProperty);
+            textbox3.ClearValue(TextBox.BorderBrushProperty);
+
+            if (!success1 || !success2 || !success3)
             {
-                MessageBox.Show("請輸入整數", "輸入錯誤");
+                MessageBox.Show("請輸入有效的數值", "輸入錯誤");
+                if (!success1)
+                    textbox1.BorderBrush = Brushes.Red;
+                if (!success2)
+                    textbox2.BorderBrush = Brushes.Red;
+                if (!success3)
+                    textbox3.BorderBrush = Brushes.Red;
+                return;
             }
-            else if (number < 2)
+            if (side1 < 1 || side2 < 1 || side3 < 1)
             {
-                MessageBox.Show($"輸入數值為{number}，小於2，請重新輸入", "輸入錯誤");
+                MessageBox.Show("請輸入有效的數值", "輸入錯誤");
+                if (side1<1)
+                    textbox1.BorderBrush = Brushes.Red;
+                if (side2 < 1)
+                    textbox2.BorderBrush = Brushes.Red;
+                if (side3 < 1)
+                    textbox3.BorderBrush = Brushes.Red;
+                return;
+            }
+                if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1)
+            {
+                textblock_multiple.Background = new SolidColorBrush(Colors.Green);
+                textblock_multiple.Text = $"邊長 {side1}, {side2}, {side3} 可構成三角形";
+                triangles.Add(new Triangle() { Side1=side1,Side2=side2,Side3=side3,IsValid=true});
             }
             else
             {
-                //MessageBox.Show($"輸入數值為{number}", "輸入正確");
-                for (int i = 2; i <= number; i++)
-                {
-                    if (IsPrime(i))
-                    {
-                        primes.Add(i);
-                    }
-                }
+                textblock_multiple.Background = new SolidColorBrush(Colors.Red);
+                textblock_multiple.Text = $"邊長 {side1}, {side2}, {side3} 不可構成三角形";
+                triangles.Add(new Triangle() { Side1 = side1, Side2 = side2, Side3 = side3, IsValid = false });
             }
-
-            //列印所有的質數與倍數
-            ListResult(primes, number);
+            ListResult();
         }
 
-        private void ListResult(List<int> myPrimes, int n)
+        private void ListResult()
         {
-            string primeList = $"小於{n}的質數為：";
-            string primeMultiple = "";
-            foreach (int p in myPrimes)
-            {
-                primeList += $"{p}  ";
-                primeMultiple += $"{p}的倍數為： ";
-                int i = 1;
-                while (p*i <= n)
-                {
-                    primeMultiple += $"{p * i}  ";
-                    i++;
-                }
-                primeMultiple += "\n";
+            textblock_multiple2.Clear();
+            foreach(Triangle tri in triangles) {
+                textblock_multiple2.Text += $"{tri.Side1}\t{tri.Side2}\t{tri.Side3}\t{tri.IsValid}\n";
             }
-            textblock_prime.Text = primeList;
-            textblock_multiple.Text = primeMultiple;
-        }
-
-        private bool IsPrime(int p)
-        {
-            //判斷p是否為質數
-            for (int i = 2; i < p; i++)
-            {
-                if (p % i == 0) return false;
-            }
-            return true;
         }
     }
 }
