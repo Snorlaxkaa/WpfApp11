c28f872782f8a8c0c171a7c0fb5d7a18da82639c
宗彥 黃 <5b0g0017@office.stust.edu.tw>
2023-09-25 16:40:52 +0800

    0

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -7,23 +7,26 @@ using static WpfApp1.MainWindow;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        // 定義表示三角形的類別
         public class Triangle
         {
-            public double Side1 { set;  get; }
-            public double Side2 { set;  get; }
+            public double Side1 { set; get; }
+            public double Side2 { set; get; }
             public double Side3 { set; get; }
             public bool IsValid { set; get; }
         }
-        List<Triangle> triangles=new List<Triangle>();
+
+        // 存儲三角形的列表
+        List<Triangle> triangles = new List<Triangle>();
+
+        // 點擊按鈕事件處理程序
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             double side1, side2, side3;
@@ -31,13 +34,14 @@ namespace WpfApp1
             bool success2 = double.TryParse(textbox2.Text, out side2);
             bool success3 = double.TryParse(textbox3.Text, out side3);
 
-            // 清除之前可能设置的边框颜色
+            // 清除之前可能設置的文本框邊框顏色
             textbox1.ClearValue(TextBox.BorderBrushProperty);
             textbox2.ClearValue(TextBox.BorderBrushProperty);
             textbox3.ClearValue(TextBox.BorderBrushProperty);
 
             if (!success1 || !success2 || !success3)
             {
+                // 如果輸入無效，顯示錯誤消息並突出顯示錯誤的文本框
                 MessageBox.Show("請輸入有效的數值", "輸入錯誤");
                 if (!success1)
                     textbox1.BorderBrush = Brushes.Red;
@@ -47,10 +51,12 @@ namespace WpfApp1
                     textbox3.BorderBrush = Brushes.Red;
                 return;
             }
+
             if (side1 < 1 || side2 < 1 || side3 < 1)
             {
+                // 如果輸入小於1，顯示錯誤消息並突出顯示錯誤的文本框
                 MessageBox.Show("請輸入有效的數值", "輸入錯誤");
-                if (side1<1)
+                if (side1 < 1)
                     textbox1.BorderBrush = Brushes.Red;
                 if (side2 < 1)
                     textbox2.BorderBrush = Brushes.Red;
@@ -58,25 +64,35 @@ namespace WpfApp1
                     textbox3.BorderBrush = Brushes.Red;
                 return;
             }
-                if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1)
+
+            if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1)
             {
+                // 如果三邊長可以構成三角形，將結果設置為綠色並添加到列表
                 textblock_multiple.Background = new SolidColorBrush(Colors.Green);
                 textblock_multiple.Text = $"邊長 {side1}, {side2}, {side3} 可構成三角形";
-                triangles.Add(new Triangle() { Side1=side1,Side2=side2,Side3=side3,IsValid=true});
+                triangles.Add(new Triangle() { Side1 = side1, Side2 = side2, Side3 = side3, IsValid = true });
             }
             else
             {
+                // 如果三邊長不能構成三角形，將結果設置為紅色並添加到列表
                 textblock_multiple.Background = new SolidColorBrush(Colors.Red);
                 textblock_multiple.Text = $"邊長 {side1}, {side2}, {side3} 不可構成三角形";
                 triangles.Add(new Triangle() { Side1 = side1, Side2 = side2, Side3 = side3, IsValid = false });
             }
+
+            // 更新結果列表
             ListResult();
         }
 
+        // 更新結果列表的方法
         private void ListResult()
         {
+            // 清除之前的結果
             textblock_multiple2.Clear();
-            foreach(Triangle tri in triangles) {
+
+            // 將每個三角形的信息添加到結果列表中
+            foreach (Triangle tri in triangles)
+            {
                 textblock_multiple2.Text += $"{tri.Side1}\t{tri.Side2}\t{tri.Side3}\t{tri.IsValid}\n";
             }
         }
